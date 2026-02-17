79fc7399f58d68fe04ce59c40d4ae69fac400d93
foxstudent100683 <anton.matsibora@gmail.com>
2026-02-17 11:40:15 +0100

    Added Template Form

---
+++ src/main/java/cc/hiknomore/questions/model/TemplateForm.java
@@ -0,0 +1,10 @@
+package cc.hiknomore.questions.model;
+
+public class TemplateForm {
+    
+    private Form templateForm;
+    
+    public void AddQuestion() {}
+    
+    public void RemoveQuestion() {}
+}
